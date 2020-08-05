882f796269da0fa982c29994083dfe138aec0d8e
Graham Howden <graham_howden1@yahoo.co.uk>
2020-08-05 12:52:09 +0100

    turns out including $ in a replaceAll was a bad idea 🤔

--- src/main/scala/digital/and/challenges/two/Main.scala
+++ src/main/scala/digital/and/challenges/two/Main.scala
@@ -4,10 +4,8 @@ import scala.io.Source
 
 // "Some people, when confronted with a problem, think “I know, I'll use regular expressions.” Now they have two problems."
 object Main extends App {
-  val output = Source.fromResource("challenge-two.txt").getLines().next().replaceAll(""".{1,32}(?<=\s|\S-|$)""", "$0\n")
+  val output = Source.fromResource("challenge-two.txt").getLines().next().replaceAll(""".{1,32}(?<=\s|\S-)""", "$0\n")
 
-  // this answer comes out at 64, which was 1 off of the submit answer but idk why 🤔,
-  // removing the newlines ends up with the original string and no lines are above 32
   println(output, output.linesIterator.count(_.length == 32))
 }
 
@@ -15,11 +13,11 @@ object Main extends App {
 //
 // .{1,32}       # 1 up to 32 of any character
 // (?<=          # look behind (basically previous must end in this regex):
-//   \s|\S-|$    # either a whitespace char, a non-whitespace char followed by a hypen, or the end of the string
+//   \s|\S-      # either a whitespace char, a non-whitespace char followed by a hypen
 // )
 //
 // we then replace matches in the string with $0\n, meaning whatever was matched then a newline
 //
-// alternative: (.{1,32})(?:\s+|(?:<=\S-)|$) -> $1\n
+// alternative: (.{1,32})(?:\s+|(?:<=\S-)) -> $1\n
 // same but without the trailing whitespace being counted as part of the 32 and also then not included in output, makes
 // for some more 'full' lines of 32 characters and a harder to understand regex
